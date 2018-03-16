d56a42e0e5825a17f6faaf4ca712ae697aeea023
Dennis Schroer <dennis.schroer@topicus.nl>
2018-03-16 16:52:34 +0100

    Fix compilation issues

--- src/main/groovy/nl/topicus/overheid/javafactorybot/dsl/AttributesDsl.groovy
+++ src/main/groovy/nl/topicus/overheid/javafactorybot/dsl/AttributesDsl.groovy
@@ -9,38 +9,43 @@ import nl.topicus.overheid.javafactorybot.definition.ManyAssociation
 import nl.topicus.overheid.javafactorybot.definition.ValueAttribute
 
 trait AttributesDsl {
-    static ValueAttribute attribute(
-            @DelegatesTo(Evaluator) @ClosureParams(value = SimpleType, options = "Evaluator") Closure defaultValueGenerator) {
+    /**
+     * Creates a new attribute which either uses the user specified override or the result of the closure.
+     *
+     * @param defaultValueGenerator The closure which generates a value when no override is given.
+     * @return An attribute which resolves to the override or the result of the closure.
+     */
+    ValueAttribute attribute(@DelegatesTo(Evaluator) @ClosureParams(value = SimpleType, options = "Evaluator") Closure defaultValueGenerator) {
         new ValueAttribute(defaultValueGenerator)
     }
 
-    static <T> Association<T> hasOne(Factory<T> factory) {
-        new Association<T>(factory)
+    def <T> Association<T> hasOne(Factory<T> factory) {
+        new Association<>(factory)
     }
 
-    static <T> Association<T> hasOne(Factory<T> factory, T defaultObject) {
-        new Association<T>(factory, defaultObject)
+    def <T> Association<T> hasOne(Factory<T> factory, T defaultObject) {
+        new Association<>(factory, defaultObject)
     }
 
-    static <T> Association<T> hasOne(Factory<T> factory, Map<String, Object> overrides, List<String> traits = null) {
-        new Association<T>(factory, overrides, traits)
-    }
-
-    // Special version for groovy syntax
-    static <T> Association<T> hasOne(Map<String, Object> overrides, Factory<T> factory, List<String> traits = null) {
-        new Association<T>(factory, overrides, traits)
-    }
-
-    static <T> ManyAssociation<T> hasMany(Factory<T> factory, int amount, Map<String, Object> overrides = null, List<String> traits = null) {
-        new ManyAssociation<T>(factory, amount, overrides, traits)
+    def <T> Association<T> hasOne(Factory<T> factory, Map<String, Object> overrides, List<String> traits = null) {
+        new Association<>(factory, overrides, traits)
     }
 
     // Special version for groovy syntax
-    static <T> ManyAssociation<T> hasMany(Map<String, Object> overrides, Factory<T> factory, int amount, List<String> traits = null) {
-        new ManyAssociation<T>(factory, amount, overrides, traits)
+    def <T> Association<T> hasOne(Map<String, Object> overrides, Factory<T> factory, List<String> traits = null) {
+        new Association<>(factory, overrides, traits)
     }
 
-    static <T> ManyAssociation<T> hasMany(Factory<T> factory, List<Object> defaultOverrides, List<String> traits = null) {
-        new ManyAssociation<T>(factory, defaultOverrides, traits)
+    def <T> ManyAssociation<T> hasMany(Factory<T> factory, int amount, Map<String, Object> overrides = null, List<String> traits = null) {
+        new ManyAssociation<>(factory, amount, overrides, traits)
+    }
+
+    // Special version for groovy syntax
+    def <T> ManyAssociation<T> hasMany(Map<String, Object> overrides, Factory<T> factory, int amount, List<String> traits = null) {
+        new ManyAssociation<>(factory, amount, overrides, traits)
+    }
+
+    def <T> ManyAssociation<T> hasMany(Factory<T> factory, List<Object> defaultOverrides, List<String> traits = null) {
+        new ManyAssociation<>(factory, defaultOverrides, traits)
     }
 }
