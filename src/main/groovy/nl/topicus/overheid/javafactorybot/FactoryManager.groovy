34a725a75646e4ce99b5cd463b9e1ee8c41ab345
Dennis Schroer <dennis.schroer@topicus.nl>
2018-04-06 13:40:38 +0200

    Add support for the @Singleton annotation when creating a factory instance

--- src/main/groovy/nl/topicus/overheid/javafactorybot/FactoryManager.groovy
+++ src/main/groovy/nl/topicus/overheid/javafactorybot/FactoryManager.groovy
@@ -2,6 +2,8 @@ package nl.topicus.overheid.javafactorybot
 
 import com.github.javafaker.Faker
 
+import java.lang.reflect.Method
+
 /**
  * Global manager for the factories which can be used to manage the factories. This manager is a singleton and can contain shared configuration
  * between factories.
@@ -39,10 +41,43 @@ class FactoryManager {
 
     private Map<Class<BaseFactory<?, ? extends Faker>>, ? extends BaseFactory<?, ? extends Faker>> factoryInstances = [:]
 
+    /**
+     * Gets or creates an instance of the given factory type.
+     *
+     * The default implementation keeps a single instance per factory type. A new instance is created
+     * by calling {@link #newFactoryInstance(java.lang.Class)}}.
+     *
+     * @param factoryClass The type of the factory to get or create an instance of.
+     * @return An instance of the factory type.
+     */
     def <M, F extends Faker, T extends BaseFactory<M, F>> T getFactoryInstance(Class<T> factoryClass) {
         if (!factoryInstances.containsKey(factoryClass)) {
-            factoryInstances.put(factoryClass, factoryClass.newInstance())
+            factoryInstances.put(factoryClass, newFactoryInstance(factoryClass))
         }
-        factoryInstances[factoryClass] as T
+        factoryInstances[factoryClass as Class<BaseFactory<?, ? extends Faker>>] as T
+    }
+
+    /**
+     * Creates a new instance of the given factory type.
+     *
+     * The default implementation looks if a method called 'getInstance' is defined on the class. If such
+     * a method exists, this method is invoked. Otherwise, the default constructor is used.
+     *
+     * This implementation exists so the annotation {@link Singleton} can be applied to factories.
+     *
+     * @param factoryClass The type of the factory to create an instance of
+     * @return A new instance of the factory type.
+     */
+    def <M, F extends Faker, T extends BaseFactory<M, F>> T newFactoryInstance(Class<T> factoryClass) {
+        T factory = null
+        try{
+            Method method = factoryClass.getMethod("getInstance")
+            if(method != null){
+                factory = method.invoke(null) as T
+            }
+        } catch(NoSuchMethodException ignored) {
+            // Do nothing
+        }
+        factory ?: factoryClass.newInstance()
     }
 }
